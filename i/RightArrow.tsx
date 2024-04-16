d80da0730e2ba835cd2d72f8f716f0ffc31b3d4e
verrixkio <verrixkio@gmail.com>
2024-04-16 09:03:05 -0600

    finish hero block for desktop + mobile

---
+++ i/RightArrow.tsx
@@ -0,0 +1,19 @@
+<svg
+  width="20"
+  height="20"
+  viewBox="0 0 20 20"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <g clipPath="url(#clip0_109_3139)">
+    <path
+      d="M9.99992 3.3335L8.82492 4.5085L13.4749 9.16683H3.33325V10.8335H13.4749L8.82492 15.4918L9.99992 16.6668L16.6666 10.0002L9.99992 3.3335Z"
+      fill="#FFFFFF"
+    />
+  </g>
+  <defs>
+    <clipPath id="clip0_109_3139">
+      <rect width="20" height="20" fill="white" />
+    </clipPath>
+  </defs>
+</svg>;
